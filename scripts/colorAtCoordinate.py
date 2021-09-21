bb6f5eafaeb6eea0fb7827526187636b3cd16ce1
geva308 <geva308@gmail.com>
2021-09-21 13:16:52 +0300

    colorAtCoordinate script

--- scripts/colorAtCoordinate.py
+++ scripts/colorAtCoordinate.py
@@ -1,8 +1,20 @@
 # pip install numpy
 import numpy as np
-import cv2
+from PIL import Image
+import matplotlib.pyplot as pyplot
 
 # colorAtCoordinate
 
-def colorAtCoordinate():
-    # TODO
+def colorAtCoordinate(image_np_array,row,col):
+    r,g,b = image_np_array[row,col]
+    return (r,g,b)
+
+
+
+
+# calling the method
+image_path = '../resources/cast.jpg'
+open_image = Image.open(image_path)
+image_as_npArray = np.array(open_image)
+res=colorAtCoordinate(image_as_npArray,100,90)
+print(res)
